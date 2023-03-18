2078df62cf3585e78390e7c41d26f66664917ac1
HEGADE <subramanyashegade@gmail.com>
2023-03-18 22:56:35 +0530

    refactor: changed  the name of the apps unnder /apps

---
+++ apps/admin-web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+      <h1>app used by user</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ apps/user-web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>app used by the the admin and guide</h1>
+      <Button />
+    </div>
+  );
+}
