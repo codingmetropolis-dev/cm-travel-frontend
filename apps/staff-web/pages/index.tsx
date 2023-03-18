cd9c8d634a2720b5992db8c8b230fe5822613fa5
HEGADE <subramanyashegade@gmail.com>
2023-03-18 23:10:52 +0530

    refactor: changed  the name of the apps under /apps

--- apps/staff-web/pages/index.tsx
+++ apps/staff-web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>app used by user</h1>
+      <h1>app used by guide</h1>
       <Button />
     </div>
   );
--- apps/traveller-web/pages/index.tsx
+++ apps/traveller-web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Docs() {
   return (
     <div>
-      <h1>app used by the the admin and guide</h1>
+      <h1>app used by the the customer</h1>
       <Button />
     </div>
   );
